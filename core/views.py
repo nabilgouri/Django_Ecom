cbdcd819ab24d20b1078b9eb882090234d0bd454
nabilgouri <38146132+nabilgouri@users.noreply.github.com>
2020-12-10 17:08:35 +0100

    fixed the bug of quantity fel cart

--- core/views.py
+++ core/views.py
@@ -32,7 +32,9 @@ def add_to_cart(request, slug):
     # hna we get the item lel cart
     item = get_object_or_404(Item, slug=slug)
     # hna we make it a part f our order
-    order_item = OrderItem.objects.create(item=item)
+    order_item, created = OrderItem.objects.get_or_create(
+        item=item, user=request.user, ordered=False)
+
     # order_qs tessma order querry set w nfiltriwha to be unique 3a assass l user ofc
     order_qs = Order.objects.filter(user=request.user, ordered=False)
     if order_qs.exists():
@@ -42,6 +44,8 @@ def add_to_cart(request, slug):
         if order.items.filter(item__slug=item.slug).exists():
             order_item.quantity = order_item.quantity + 1
             order_item.save()
+        else:
+            order.items.add(order_item)
 
     else:
         ordered_date = timezone.now()
